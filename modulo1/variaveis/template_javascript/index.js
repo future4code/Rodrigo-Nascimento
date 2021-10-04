f703ca9393b4fb292a164b6f84da634d8df1cd5d
desatinar <rodrigosdn@gmail.com>
2021-10-04 13:13:51 -0300

    exercicios da aula sobre variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,56 @@
+/* 
+Exercícios de interpretação de código
+
+1. 10
+   10 5
+   
+2. 10 10 10
+
+3. let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+   let salarioDia = prompt("Quanto você recebe por dia?")
+   alert(`Voce recebe ${salarioDia/horasTrabalhadasDia} por hora`)
+
+Exercícios de escrita de código
+
+1. 
+a) let nome
+b) let idade
+c) console.log(typeof nome, typeof idade)
+d) Ambos voltaram como undefined pq nenhuma das duas variáveis tiveram valores definidos.
+e) let nome = prompt("Qual é o seu nome?")
+   let idade = prompt("Qual é a sua idade?")
+
+f) console.log(typeof nome, typeof idade)
+                    string string
+
+g) let nome = prompt("Qual é o seu nome?")
+   let idade = prompt("Qual é a sua idade?")
+   console.log("Olá,", nome, "você tem", idade, "anos.")
+   
+2.
+const resposta1 = prompt("Hoje é segunda feira?")
+console.log("Hoje é segunda feira? -", resposta1)
+const resposta2 = prompt("Laranja é laranja?")
+console.log("Laranja é laranja? -", resposta2)
+const resposta3 = prompt("Cachorro é um animal?")
+console.log("Cachorro é um animal? -", resposta3)
+
+3.
+let a = 10
+let b = 25
+let c = a
+
+console.log("O novo valor de a é", b) 
+console.log("O novo valor de b é", c)
+
+DESAFIO
+
+let primeiroNumero = prompt("Digite o primeiro número")
+let segundoNumero = prompt("Digite o segundo número")
+let soma = Number(primeiroNumero) + Number(segundoNumero)
+let multiplicacao = Number(primeiroNumero) * Number(segundoNumero)
+
+console.log("O primeiro número somado ao segundo número resulta em:", soma)
+console.log("O primeiro número multiplicado pelo segundo número resulta em:", multiplicacao)
+*/
+
