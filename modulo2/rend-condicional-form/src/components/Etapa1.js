7ea2bb6a2c5adf52935b3227a5f82f49ca2fbb09
desatinar <rodrigosdn@gmail.com>
2021-11-08 23:37:20 -0300

    Exercício Renderização Condicional

---
+++ modulo2/rend-condicional-form/src/components/Etapa1.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default class Etapa1 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>ETAPA 1 - DADOS GERAIS</h1>
+                <p>1. Qual o seu nome?</p>
+                <input/>
+                <p>2. Qual a sua idade?</p>
+                <input/>
+                <p>3. Qual seu email?</p>
+                <input/>
+                <p>4. Qual a sua escolaridade?</p>
+                <select>
+                    <option>Ensino médio incompleto</option>
+                    <option>Ensino médio completo</option>
+                    <option>Ensino superior incompleto</option>
+                    <option>Ensino superior completo</option>
+                </select>
+            </div>
+
+        );
+    }
+}
+
+
+
---
+++ modulo2/rend-condicional-form/src/components/Etapa4.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class Etapa4 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>O FORMULÁRIO ACABOU</h1>
+                <p>Muito Obrigado por participar! Entraremos em contato!</p>
+            </div>
+        )
+    }
+}
