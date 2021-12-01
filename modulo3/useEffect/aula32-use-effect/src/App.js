0c855f3102e067286373853d839df20b75b69d6b
desatinar <rodrigosdn@gmail.com>
2021-11-30 22:18:40 -0300

    exercício useEffect

---
+++ modulo3/useEffect/aula32-use-effect/src/App.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PokeCard from "./components/PokeCard/PokeCard";
+
+function App () {
+
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
+
+  useEffect(() => {
+  
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        setPokeList(response.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  }
+
+    return (
+      <div>
+        <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
+      </div>
+    );
+  }
+
+
+    
+
+
+export default App;
