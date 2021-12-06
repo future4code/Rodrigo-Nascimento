da6a6821035d3d55173d4a3319a5dfdc1a1bac41
desatinar <rodrigosdn@gmail.com>
2021-12-06 18:10:09 -0300

    projeto labex

---
+++ modulo3/labex/src/App.js
@@ -0,0 +1,15 @@
+
+import Button from '@mui/material/Button'
+
+function App() {
+  return (
+    <div >
+      <Button variant="contained">
+        teste
+      </Button>
+      olar
+    </div>
+  );
+}
+
+export default App;
