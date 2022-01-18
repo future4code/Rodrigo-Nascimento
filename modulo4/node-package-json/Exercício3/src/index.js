b92aa12f1e0a71d12310bbac9ffdd607ee5d7f5d
desatinar <rodrigosdn@gmail.com>
2022-01-18 18:37:11 -0300

    exercicio node e package.json

---
+++ modulo4/node-package-json/Exercício3/src/index.js
@@ -0,0 +1,11 @@
+// Exercício
+
+// 3
+
+const listaTarefas = ["Almoçar", "Estudar", "Fazer exercício"]
+
+listaTarefas.push(process.argv[2])
+
+console.log(`Tarefa ${process.argv[2]} adicionada com sucesso!`, `Suas tarefas:`, listaTarefas)
+
+
