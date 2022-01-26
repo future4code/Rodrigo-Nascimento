63261a1c5b0b233abee69298b91d13322d691f96
desatinar <rodrigosdn@gmail.com>
2022-01-26 15:33:36 -0300

    criado o array de produtos do exercicio 2

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,59 @@
+type Produto = {
+  id: string,
+  name: string,
+  price: number
+}
+
+const produto: Produto[] = [
+  {
+    id: Date.now().toString(),
+    name: "Mamão",
+    price: 1
+  },
+  {
+    id: Date.now().toString(),
+    name: "Melão",
+    price: 2
+  },
+  {
+    id: Date.now().toString(),
+    name: "Melancia",
+    price: 3
+  },
+  {
+    id: Date.now().toString(),
+    name: "Limão",
+    price: 4
+  },
+  {
+    id: Date.now().toString(),
+    name: "Laranja",
+    price: 5
+  },
+  {
+    id: Date.now().toString(),
+    name: "Maçã",
+    price: 6
+  },
+  {
+    id: Date.now().toString(),
+    name: "Uva",
+    price: 7
+  },
+  {
+    id: Date.now().toString(),
+    name: "Manga",
+    price: 8
+  },
+  {
+    id: Date.now().toString(),
+    name: "Jambo",
+    price: 9
+  },
+  {
+    id: Date.now().toString(),
+    name: "Acerola",
+    price: 10
+  },
+]
+
