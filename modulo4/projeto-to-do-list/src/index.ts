2bd0f1ff864a0e7dd7d4bfa5cee7939bdeca5c6c
desatinar <rodrigosdn@gmail.com>
2022-02-04 20:03:12 -0300

    exercicio feito ate o desafio 8

--- modulo4/projeto-to-do-list/src/index.ts
+++ modulo4/projeto-to-do-list/src/index.ts
@@ -18,6 +18,191 @@ const server = app.listen(process.env.port || 3003, () => {
   }
 })
 
+
+//Pesquisar usuário
+app.get("/user", async (req: Request, res: Response): Promise<void> => {
+  let codeError = 400
+  try {
+    const query: string = req.query.query as string
+    
+    if(query?.length === 0){
+      codeError = 422
+      throw new Error("Informe um termo de pesquisa válido")
+    }
+
+    const result = await connection("ToDoList")
+      .select("id", "nickname")
+
+    const filterUser = result.filter((user) => {
+      if(query === user.email|| query === user.nickname){
+        return ({
+            id: user.id,
+            nickname: user.nickname
+        })
+      }
+    })
+
+    if(filterUser.length === 0){
+      res.send({users: []})
+    }
+
+    res.status(201).send({users: filterUser})
+
+  } catch (error: any) {
+    res.status(codeError).send(error.message)
+  }
+})
+
+//Pegar todos os usuários
+app.get("/user/all", async (req: Request, res: Response): Promise<void> => {
+
+  const result = await connection("ToDoList")
+    .select("id", "nickname")
+  
+  if(result.length === 0){
+    res.status(200).send([])
+  } else {
+    res.status(201).send({users: result })
+  }
+})
+
+//Pegar usuário pelo id
+app.get("/user/:id", async (req: Request, res: Response): Promise<void> => {
+  let codeError = 400
+  try {
+    const id: string = req.params.id
+
+    if (id === ":id") {
+      codeError = 422
+      throw new Error("Informe um ID válido")
+    }
+
+    const result = await connection("ToDoList")
+      .select("id", "nickname")
+      .where("id", id)
+
+    console.log(result, "result")
+
+    if (result.length === 0) {
+      codeError = 422
+      throw new Error("ID não encontrado")
+    }
+
+    res.send(result)
+
+  } catch (error: any) {
+    res.status(codeError).send(error.message || error.sqlMessage)
+  }
+})
+
+//Pegar tarefas criadas por um usuário
+app.get("/task", async (req: Request, res: Response): Promise<void> =>{
+  let codeError = 400
+  try {
+    const creatorUserId: string = req.query.creatorUserId as string
+
+    const result = await connection("ToDoListTask")
+      .select()
+      .join("ToDoList", "ToDoList.id", "ToDoListTask.creator_user_id")
+      .where("ToDoListTask.creator_user_id", creatorUserId)
+
+    const newData = result.map((task) =>{
+      return({
+        taskId: task.id,
+        title: task.title,
+        description: task.description,
+        limitDate: task.limit_date.toLocaleString().slice(0, -9),
+        status: task.status,
+        creatorUserId: task.creator_user_id,
+        creatorUserNickname: task.nickname
+      }) 
+    })
+
+    if(result.length === 0){
+      codeError = 422
+      throw new Error("Nenhuma tarefa encontrada");
+    } else {
+      res.status(201).send(newData)
+    }
+
+  } catch (error: any) {
+    res.status(codeError).send(error.message)
+  }
+
+})
+
+//Pegar tarefa pelo id
+app.get("/task/:id", async (req: Request, res: Response): Promise<void> => {
+  let codeError = 400
+  try {
+    const id: string = req.params.id
+ 
+    const result = await connection("ToDoListTask")
+      .select("ToDoListTask.id as id", "title", "description", "limit_date as limitDate", "status", "creator_user_id as creatorUserId", "nickname as creatorUserNickname")
+      .join("ToDoList", "ToDoList.id", "ToDoListTask.creator_user_id")
+      .where("ToDoListTask.id", id)
+
+    if(result.length === 0){
+      codeError = 422
+      throw new Error("Task não encontrada")
+    }
+
+    const newData = result.map((data) =>{
+      return ({
+        taskId: data.id,
+        title: data.title,
+        description: data.description,
+        limitDate: data.limitDate.toLocaleString().slice(0, -9),
+        status: data.status,
+        creatorUserId: data.creatorUserId,
+        creatorUserNickname: data.creatorUserNickname
+        })    
+    })
+
+    res.status(201).send(newData)
+
+  } catch (error: any) {
+    res.status(codeError).send(error.message || error.sqlMessage)
+  }
+})
+
+//Criar tarefa
+app.post("/task", async (req: Request, res: Response): Promise<void> => {
+  let codeError = 400
+  try {
+    const { title, description, limitDate, creatorUserId }: { title: string, description: string, limitDate: string, creatorUserId: string } = req.body
+    const taskId: string = Date.now().toString()
+
+    if (!title || !description || !limitDate || !creatorUserId) {
+      codeError = 422
+      throw new Error("Dados inválidos")
+    }
+
+    const changeDate = (date: string) => {
+      const newDate = date.split("/")
+      return newDate[2] + "-" + newDate[1] + "-" + newDate[0] as string
+    }
+
+    const newDate = changeDate(limitDate)
+
+    const result = await connection("ToDoListTask")
+      .insert({
+        id: taskId,
+        title: title,
+        description: description,
+        limit_date: newDate,
+        creator_user_id: creatorUserId
+      })
+
+    res.send("Tarefa criada com sucesso!")
+
+  } catch (error: any) {
+    res.status(codeError).send({ result: error.message || error.sqlMessage })
+  }
+
+})
+
+//Criar usuário
 app.post("/user", async (req: Request, res: Response): Promise<void> => {
   let codeError = 400
   try {
@@ -37,39 +222,14 @@ app.post("/user", async (req: Request, res: Response): Promise<void> => {
         email
       })
 
-    res.status(201).send("Pessoa cadastrada com sucesso!")
+    res.status(201).send("Pessoa criada com sucesso!")
 
   } catch (error: any) {
     res.status(codeError).send(error.message || error.sqlMessage)
   }
 })
 
-app.get("/user/:id", async (req: Request, res: Response): Promise<void> => {
-  let codeError = 400
-  try {
-    const id: string = req.params.id
-
-    if (!id) {
-      codeError = 422
-      throw new Error("Informe um ID válido")
-    }
-
-    const result = await connection("ToDoList")
-      .select("id", "nickname")
-      .where("id", id)
-
-    if(result.length === 0){
-      codeError = 422
-      throw new Error("ID não encontrado")
-    }
-
-    res.send(result)
-
-  } catch (error: any) {
-    res.status(codeError).send(error.message || error.sqlMessage)
-  }
-})
-
+// Editar usuário
 app.put("/user/edit/:id", async (req: Request, res: Response): Promise<void> => {
   let codeError = 400
   try {
@@ -82,7 +242,7 @@ app.put("/user/edit/:id", async (req: Request, res: Response): Promise<void> => 
     }
 
     const result = await connection("ToDoList")
-      .update({name, nickname, email})
+      .update({ name, nickname, email })
       .where("id", id)
 
     res.status(201).send("Alterações feitas com sucesso!")
@@ -90,42 +250,5 @@ app.put("/user/edit/:id", async (req: Request, res: Response): Promise<void> => 
   } catch (error: any) {
     res.status(codeError).send(error.message || error.sqlMessage)
 
-  }
-})
-
-app.post("/task", (req: Request, res: Response) => {
-  let codeError = 400
-  try {
-    const { title, description, limitDate, creatorUserId }: { title: string, description: string, limitDate: string, creatorUserId: string } = req.body
-    const taskId: string = Date.now().toString()
-
-    if (!title || !description || !limitDate || !creatorUserId) {
-      codeError = 422
-      throw new Error("Dados inválidos")
-    }
-
-    const changeDate = (date: string) => {
-      const newDate = date.split("/")
-      return newDate[2] + "-" + newDate[1] + "-" + newDate[0] as string
-    }
-
-    const data = "30/01/2003"
-    console.log(changeDate(data))
-
-    res.send({ title, description, limitDate, creatorUserId })
-
-  } catch (error: any) {
-    res.status(codeError).send(error.message || error.sqlMessage)
-  }
-
-})
-
-app.get("/task/:id", (req: Request, res: Response) => {
-  let codeError = 400
-  try {
-
-
-  } catch (error: any) {
-    res.status(codeError).send(error)
   }
 })
